a2ef3f729833e29fe4448ff5fed8eecc50e0810c
danielam <daniela.miron.dumitrascu@external.toyota-europe.com>
2024-10-16 13:45:35 +0300

    Comit Variables

---
+++ src/main/java/org/JavaAutomation/variableandcomments/VariableAndComments.java
@@ -0,0 +1,42 @@
+package org.JavaAutomation.variableandcomments;
+
+public class VariableAndComments {
+    public static void main(String[] args) {
+        // Declare the primitive data type variable
+
+        boolean falseBooleanVariable;
+        boolean trueBooleanVariable;
+        char firstCharFromAscii;
+        char lastCharFromAscii;
+        byte lowerLimitOfByteDataType;
+        byte upperLimitOfByteDataType;
+        short lowerLimitOfShortType;
+        short upperLimitOfShortType;
+        int lowerLimitOfIntDataType;
+        int upperLimitOfIntDataType;
+        long lowerLimitOfLongDataType;
+        long upperLimitOfLongDataType;
+        float lowerLimitOfFloatDataType;
+        float upperLimitOfFloatDataType;
+        double lowerLimitOfDoubleDataType;
+        double upperLimitOfDoubleDataType;
+
+        // Initialisation of the above variables
+
+        falseBooleanVariable =false;
+        trueBooleanVariable =true;
+        lowerLimitOfByteDataType = -128;
+        upperLimitOfByteDataType = 127;
+        lowerLimitOfShortType = -32768;
+        upperLimitOfShortType =32767;
+        lowerLimitOfIntDataType = -2147483648;
+        upperLimitOfIntDataType =2147483647;
+        lowerLimitOfLongDataType = -9223372036854775808L;
+        upperLimitOfLongDataType = 9223372036854775807L;
+        lowerLimitOfFloatDataType =-3.4E+38f;
+        upperLimitOfFloatDataType = 3.4E+38f;
+        lowerLimitOfDoubleDataType = -1.7E+308d;
+        upperLimitOfDoubleDataType = 1.7E+305;
+
+    }
+}
