a6eface36d75976296aa33499d063bd84b3ee456
danielam <daniela.miron.dumitrascu@external.toyota-europe.com>
2024-10-16 13:57:59 +0300

    Comit Variables

--- src/main/java/org/JavaAutomation/Main.java
+++ src/main/java/org/JavaAutomation/Main.java
@@ -2,6 +2,32 @@ package org.JavaAutomation;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        boolean numeVariabilaBoolean;
+        char numeVariabilaChar;
+        byte numeVariabilaByte;
+        short numeVariabilaShort;
+        int numeVariabilaInt;
+        long numeVariabilaLong;
+        float numeVariabilaFloat;
+        double numeVariabilaDouble;
+
+        String numeDeObiectDeTipulString;
+        Object numeObiectObject;
+
+
+        numeVariabilaBoolean = true;
+        numeVariabilaChar = 'A';
+        numeVariabilaByte = 127;
+        numeVariabilaShort = -32000;
+        numeVariabilaInt = 3522454 ;
+        numeVariabilaLong = 544854458;
+        numeVariabilaFloat = 35.4435855665f;
+        numeVariabilaDouble = 95874.258466d;
+
+         numeDeObiectObject = new Object();
+
+
+
     }
 }
