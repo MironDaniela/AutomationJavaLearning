0c8f0f4cbce017ab9767ff4f1391fc91f9ba99d1
danielam <daniela.miron.dumitrascu@external.toyota-europe.com>
2024-11-04 19:00:10 +0200

    Added the implementation of Zoo Service by using OOP

---
+++ src/main/java/org/JavaAutomation/zooclubservice/AnimalInterface.java
@@ -0,0 +1,7 @@
+package org.JavaAutomation.zooclubservice;
+public interface AnimalInterface {
+
+    // Toate Clasele care implementeaza AnimalInterface va trebui sa faca override la makesound() (adica sa implementeze metoda)
+  void makeSound();
+
+}
